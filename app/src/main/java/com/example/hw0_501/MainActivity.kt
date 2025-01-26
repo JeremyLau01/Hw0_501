136dd14749948e9aa53449d39d875917976eb653
JeremyLau01 <jeremylau01@gmail.com>
2025-01-26 12:50:08 -0500

    Centered Button when clicked displays "Hello World" text

--- app/src/main/java/com/example/hw0_501/MainActivity.kt
+++ app/src/main/java/com/example/hw0_501/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column // for centering
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf // Can't change text and update UI
+import androidx.compose.runtime.remember // Can't change text and update UI
+import androidx.compose.ui.Alignment // for centering elements
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hw0_501.ui.theme.Hw0_501Theme
@@ -19,29 +25,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Hw0_501Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ButtonHello()
             }
         }
     }
 }
 
+@Preview(showBackground = true, showSystemUi = true) // so I can see changes in preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ButtonHello() {
+    // Can't just have a val/variable and change it - for some reason doesn't work in Kotlin
+    // Can't even call another method, get error regarding calling composable within composable
+    val displayText = remember { mutableStateOf("") }
+
+    Column( // use to center button and text
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center, // Center Vertically button and text
+        horizontalAlignment = Alignment.CenterHorizontally, // Center Horizontally button and text
+    ) {
+        Button(onClick = {
+            displayText.value = "Hello World"
+        }) {
+            Text("Button")
+        }
+        DisplayText(displayText.value)
+    }
 }
 
-@Preview(showBackground = true)
+// Calling another method to display text
+// for some reason cannot get this to show up in preview, but works in build
 @Composable
-fun GreetingPreview() {
-    Hw0_501Theme {
-        Greeting("Android")
+fun DisplayText(text: String) {
+    if (text.isNotEmpty()) { // so only display text once vs creating more text every time button is clicked
+        Text(text)
     }
 }
